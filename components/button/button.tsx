ecc1218fdc810a176475e11ca331f5a2005c4910
StarkBak <sstechnologyindtec@gmail.com>
2025-02-20 22:39:32 +0430

    resume download added

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,8 +1,12 @@
 const Button: React.FC<{ text: string }> = ({ text }) => {
   return (
-    <button className="my-6 px-6 py-3 bg-blue-500 text-white rounded-lg font-medium hover:bg-blue-600 transition">
+    <a
+      href="/resume.pdf"
+      target="_blank"
+      className="my-6 px-6 py-3 bg-blue-500 text-white rounded-lg font-medium hover:bg-blue-600 transition"
+    >
       {text}
-    </button>
+    </a>
   );
 };
 
