e26d926e0721b7423f4bd69f5a0beee2084efd0d
StarkBak <sstechnologyindtec@gmail.com>
2025-02-21 20:35:07 +0430

    seo added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://starkbak.net", // Your website URL
+  generateRobotsTxt: true, // Generates robots.txt
+  sitemapSize: 5000,
+};
