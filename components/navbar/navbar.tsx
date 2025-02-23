653f182f9ffc165a2afe658bf4ae8bb952cf62dd
StarkBak <sstechnologyindtec@gmail.com>
2025-02-23 16:39:30 +0430

    New CI/CD

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const title = "Salim Baktash";
+  const title = "SalimBaktash";
   const menuItems = [
     "Home",
     "Skills",
