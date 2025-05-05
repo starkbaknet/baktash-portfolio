90822172a183c66aa6c70b923b37bc086da50ab2
StarkBak <starkbaknetlinks@gmail.com>
2025-05-05 10:22:22 +0430

    Update button.tsx

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,7 +1,7 @@
 const Button: React.FC<{ text: string }> = ({ text }) => {
   return (
     <a
-      href="/resume.pdf"
+      href="/Salim Baktash Resume.pdf"
       target="_blank"
       className="my-6 px-6 py-3 bg-blue-500 text-white rounded-lg font-medium hover:bg-blue-600 transition"
     >
